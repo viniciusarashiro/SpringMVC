05377d2ada3008de006d4127c9e82d3162afa11e
vinicius <viniciusarashiro0@gmail.com>
2023-04-20 14:33:56 -0300

    Criação da página de detalhes do professor

--- regescweb/src/main/java/br/com/codelab/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/br/com/codelab/regescweb/controllers/ProfessorController.java
@@ -7,12 +7,15 @@ import br.com.codelab.regescweb.repositories.ProfessorRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProfessorController {
@@ -49,8 +52,24 @@ public class ProfessorController {
         } else {
             Professor professor = requisicao.toProfessor();
             this.professorRepository.save(professor);
+            return new ModelAndView("redirect:/professores/" + professor.getId());
+        }
+
+    }
+
+    @GetMapping("/professores/{id}")
+    public ModelAndView show(@PathVariable Long id) {
+        Optional<Professor> optional = this.professorRepository.findById(id);
+
+        if(optional.isPresent()) {
+            Professor professor = optional.get();
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("professor", professor);
+            return mv;
+        } else {
             return new ModelAndView("redirect:/professores");
         }
+
 
     }
  }
